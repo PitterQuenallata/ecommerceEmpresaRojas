f678d6d72c0c11eb1fcba8526eb127c01166707d
CasaPitter <quenallatapitter@gmail.com>
2023-10-06 15:49:55 -0400

    navbar actualizado

--- web/views/modules/footer.php
+++ web/views/modules/footer.php
@@ -1,9 +1,74 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
+<style>
+    a:link, a:visited, a:hover, a:active {
+        text-decoration: none !important;
+        color: white  !important;
+    }
+</style>
+<div class="container-fluid bg-dark small">
+    <div class="container py -5 text-light">
+        <div class="row row-cols-1 row-cols-md-1 row-cols-lg-2">
+            <div class="col row">
+                <div class="col-12 col-sm-4 col-md-4 col-lg-4">
+                    <h4 class="lead">
+                        <a href="#" class="text-uppercase">Electricidad</a>
+                    </h4>
+                    <hr class="border-white">
+                    <ul>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+                    <h4 class="lead">
+                        <a href="#" class="text-uppercase">Transmisiones</a>
+                    </h4>
+                    <hr class="border-white">
+                    <ul>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+                    <h4 class="lead">
+                        <a href="#" class="text-uppercase">Frenos</a>
+                    </h4>
+                    <hr class="border-white">
+                    <ul>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-4 col-md-3 col-lg-4">
+                    <h4 class="lead">
+                        <a href="#" class="text-uppercase">Suspenciones</a>
+                    </h4>
+                    <hr class="border-white">
+                    <ul>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+        Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+
+</footer>
--- web/views/modules/navbar.php
+++ web/views/modules/navbar.php
@@ -1,241 +1,261 @@
-  <!-- Navbar -->
-
-  <div class="container py-2 py-lg-4">
-
-<div class="row">
-  
-  <div class="col-12 col-lg-2 mt-1">
+<!-- Navbar -->
+<style>
+    .dropdown-toggle::after {
+        display: none !important;
     
-    <div class="d-flex justify-content-center">
-      
-      <a href="<?php echo $path ?>views/assets/js/index3.html" class="navbar-brand">
-        <img src="<?php echo $path ?>views/assets/img/template/logo.png" class="brand-image img-fluid py-3 px-5 p-lg-0 pe-lg-3">
-      </a>
+    }
+    ul{
+        padding: 0;
+        list-style: none;
+        text-decoration: none;
+    }
+</style>
+<div class="container py-2 py-lg-4">
 
-    </div>
+    <div class="row">
+
+        <div class="col-12 col-lg-2 mt-1">
+
+            <div class="d-flex justify-content-center">
+
+                <a href="<?php echo $path ?>views/assets/js/index3.html" class="navbar-brand">
+                    <img src="<?php echo $path ?>views/assets/img/template/logo.png"
+                        class="brand-image img-fluid py-3 px-5 p-lg-0 pe-lg-3">
+                </a>
+
+            </div>
 
 
-  </div>
-
-  <div class="col-12 col-lg-7 col-xl-8 mt-1 px-3 px-lg-0">
-    
-    <a class="nav-link float-start" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-
-    <div class="dropdown px-1 float-start templateColor">
-
-      <a id="dropdownSubMenu1" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle text-uppercase">
-        <span class="d-lg-block d-none">Categorias<i class="ps-lg-2 fas fa-th-list"></i></span>
-        <span class="d-lg-none d-block"><i class="fas fa-th-list"></i></span>
-
-      </a>
-
-      <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-
-        <!-- Level two dropdown-->
-        <li class="dropdown-submenu dropdown-hover">
-
-          <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
-
-            <i class="fas fa-tshirt pe-2 fa-xs"></i>  Ropa
-
-          </a>
-
-          <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa para dama</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa para hombre</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa deportiva</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa infantil</a>
-            </li>
-          </ul>
-
-          <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa para dama</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa para hombre</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa deportiva</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Ropa infantil</a>
-            </li>
-          </ul>
-
-        </li>
-        <!-- End Level two -->
-
-        <li class="dropdown-submenu dropdown-hover">
-
-          <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
-
-            <i class="fas fa-shoe-prints pe-2 fa-xs"></i>  Calzado
-
-          </a>
-
-          <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado para dama</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado para hombre</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado deportiva</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado infantil</a>
-            </li>
-          </ul>
-
-          <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado para dama</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado para hombre</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado deportiva</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Calzado infantil</a>
-            </li>
-          </ul>
-
-        </li>
-        <!-- End Level two -->
-
-        <li class="dropdown-submenu dropdown-hover">
-
-          <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
-
-            <i class="fas fa-laptop pe-2 fa-xs"></i>  Tecnología
-
-          </a>
-
-          <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Telefonía Móvil</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Tebletas electrónicas</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Computadoras</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Auriculares</a>
-            </li>
-          </ul>
-
-          <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Telefonía Móvil</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Tebletas electrónicas</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Computadoras</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Auriculares</a>
-            </li>
-          </ul>
-
-        </li>
-        <!-- End Level two -->
-
-        <li class="dropdown-submenu dropdown-hover">
-
-          <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
-
-            <i class="fas fa-graduation-cap pe-2 fa-xs"></i>  Cursos
-
-          </a>
-
-          <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Desarrollo Web</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Aplicaciones Móviles</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Diseño Gráfico</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Marketing Digital</a>
-            </li>
-          </ul>
-
-          <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Desarrollo Web</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Aplicaciones Móviles</a>
-            </li>
-            <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Diseño Gráfico</a>
-            </li>
-             <li>
-              <a tabindex="-1" href="#" class="dropdown-item">Marketing Digital</a>
-            </li>
-          </ul>
-
-        </li>
-        <!-- End Level two -->
-      </ul>
-    </div>
-
-    <form class="form-inline">
-      <div class="input-group input-group w-100 me-0 me-lg-4">
-        <input class="form-control rounded-0 p-3 pe-5" type="search" placeholder="Buscar..." style="height:40px">
-        <div class="input-group-append px-2 templateColor">
-          <button class="btn btn-navbar text-white" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
         </div>
-      </div>
-    </form>
 
-  </div>
+        <div class="col-12 col-lg-7 col-md-12 col-xl-8 mt-1 px-3 px-lg-0">
 
-  <div class="col-12 col-lg-3 col-xl-2 mt-1 px-3 px-lg-0">
-    
-    <div class="my-2 my-lg-0 d-flex justify-content-center">
-      
-      <a href="#">
-        
-        <button class="bt btn-default float-start rounded-0 border-0 py-2 px-3 templateColor">
-          
-          <i class="fa fa-shopping-cart"></i>
+            <a class="nav-link float-start" data-widget="pushmenu" href="#" role="button"><i
+                    class="fas fa-bars"></i></a>
 
-        </button>
+            <div class="dropdown px-1 float-start templateColor">
 
-      </a>
+                <a id="dropdownSubMenu1" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                    class="nav-link dropdown-toggle text-uppercase">
+                    <span class="d-lg-block d-none">Categorias<i class="ps-lg-2 fas fa-th-list"></i></span>
+                    <span class="d-lg-none d-block"><i class="fas fa-th-list"></i></span>
 
-      <div class="small border float-start ps-2 pe-5 w-100">
-        
-        TU CESTA <span>0</span><br> USD $<span>0</span>
+                </a>
 
-      </div>
+                <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+
+                    <!-- 1ra Categoria-->
+                    <li class="dropdown-submenu dropdown-hover">
+
+                        <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
+
+                            <i class="fas fa-bolt pe-2 fa-xs"></i> Electricidad
+
+                        </a>
+
+                        <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none d-xxl-none d-xl-none d-md-none">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">1</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">2</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">3</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">4</a>
+                            </li>
+                        </ul>
+
+                        <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">1</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">2</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">3</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">4</a>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <!-- End Level two -->
+
+                    <li class="dropdown-submenu dropdown-hover">
+
+                        <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
+
+                            <i class="fas fa-car pe-2 fa-xs"></i> Transmisión
+
+                        </a>
+
+                        <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none d-xxl-none d-xl-none d-md-none">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado para dama</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado para hombre</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado deportiva</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado infantil</a>
+                            </li>
+                        </ul>
+
+                        <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado para dama</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado para hombre</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado deportiva</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Calzado infantil</a>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <!-- End Level two -->
+
+                    <li class="dropdown-submenu dropdown-hover">
+
+                        <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
+
+                            <i class="fas fa-car pe-2 fa-xs"></i> Frenos
+
+                        </a>
+
+                        <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none d-xxl-none d-xl-none d-md-none">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Telefonía Móvil</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Tebletas electrónicas</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Computadoras</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Auriculares</a>
+                            </li>
+                        </ul>
+
+                        <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Telefonía Móvil</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Tebletas electrónicas</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Computadoras</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Auriculares</a>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <!-- End Level two -->
+
+                    <li class="dropdown-submenu dropdown-hover">
+
+                        <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" class="dropdown-item dropdown-toggle text-uppercase">
+
+                            <i class="fas fa-car pe-2 fa-xs"></i> Suspensiones
+
+                        </a>
+
+                        <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none d-xxl-none d-xl-none d-md-none">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Desarrollo Web</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Aplicaciones Móviles</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Diseño Gráfico</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Marketing Digital</a>
+                            </li>
+                        </ul>
+
+                        <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Desarrollo Web</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Aplicaciones Móviles</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Diseño Gráfico</a>
+                            </li>
+                            <li>
+                                <a tabindex="-1" href="#" class="dropdown-item">Marketing Digital</a>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <!-- End Level two -->
+                </ul>
+            </div>
+
+            <!-- Buscador -->
+            <form class="form-inline">
+                <div class="input-group input-group w-100 me-0 me-lg-4">
+                    <input class="form-control rounded-0 p-3 pe-5" type="search" placeholder="Buscar..."
+                        style="height:40px">
+                    <div class="input-group-append px-2 templateColor">
+                        <button class="btn btn-navbar text-white" type="submit">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+
+        <!-- cesta -->
+        <div class="col-12 col-lg-3 col-xl-2 mt-1 px-3 px-lg-0">
+
+            <div class="my-2 my-lg-0 d-flex justify-content-center">
+
+                <a href="#">
+
+                    <button class="bt btn-default float-start rounded-0 border-0 py-2 px-3 templateColor">
+
+                        <i class="fa fa-shopping-cart"></i>
+
+                    </button>
+
+                </a>
+
+                <div class="small border float-start ps-2 pe-5 w-100">
+
+                    TU CESTA <span>0</span><br> Bs. $<span>0</span>
+
+                </div>
+
+            </div>
+
+        </div>
 
     </div>
-
-  </div>
-
-</div>
 
 </div>
 
